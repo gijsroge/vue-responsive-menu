bc516c9203ae72811232959f2601868e4f4b51b9
gijsroge <rogegijs@gmail.com>
2020-01-22 23:39:08 +0100

    Added Responsive Menu example (still big WIP on the design aspect)

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -10,20 +10,83 @@
 
         <h1 class="font-weight-light m-0">A flexible responsive menu</h1>
 
+        <p>
+          A renderless Vue component that will auto detect if menu items don't
+          fit and moves them to a separate dropdown. Also known as the
+          Priority+ pattern.
+        </p>
+
+        <hr class="my-7" />
+
+        <VueResponsiveMenu
+          #default="{ menuItems, moreMenuItems}"
+          :nav="navigation"
+        >
+          <ul class="mainnav bg-light list-unstyled d-flex flex-wrap">
+            <li
+              class="p-2 px-4 bg-light"
+              v-for="menuItem in menuItems"
+              :key="menuItem.id"
+            >
+              {{ menuItem.label }}
+            </li>
+
+            <li class="p-2 px-4 bg-light" v-if="moreMenuItems.length > 0">
+              {{ menuItems.length === 0 ? '☰' : 'more ↓' }}
+              <ul class="list-unstyled p-3 position-absolute bg-light">
+                <li
+                  v-for="moreMenuItem in moreMenuItems"
+                  :key="moreMenuItem.id"
+                >
+                  {{ moreMenuItem.label }} (more)
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </VueResponsiveMenu>
       </header>
     </div>
   </div>
 </template>
 
 <script>
+import VueResponsiveMenu from '../../src/index'
 export default {
-  components: {}
+  components: { VueResponsiveMenu },
+  data() {
+    return {
+      navigation: [
+        { label: 'Page 1', id: 1, link: '#1' },
+        { label: 'Page 2', id: 2, link: '#2' },
+        { label: 'Page 3', id: 3, link: '#3' },
+        { label: 'Page 4', id: 4, link: '#4' },
+        { label: 'Page 5', id: 5, link: '#5' },
+        { label: 'Page 6', id: 6, link: '#6' },
+        { label: 'Page 7', id: 7, link: '#7' }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .header {
   display: grid;
+}
+
+@keyframes menuWidth {
+  from {
+    width: 100%;
+  }
+  to {
+    width: 20%;
+  }
+}
+.mainnav {
+  resize: both;
+  overflow: auto;
+  align-items: center;
+  animation: 3s menuWidth infinite alternate ease-in-out;
 }
 .root {
   height: 100%;
